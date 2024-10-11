ead8647c4f6461d8b7871842febf2a18ea96c96a
Thomas Garayua <tgarayua@gmail.com>
2024-10-11 19:57:07 -0400

    first commit

---
+++ DailyStride/Model/Quote.swift
@@ -0,0 +1,20 @@
+//
+//  Quote.swift
+//  DailyStride
+//
+//  Created by Thomas Garayua on 10/11/24.
+//
+
+import Foundation
+
+struct Quote: Codable {
+    var q: String
+    var a: String
+    var h: String
+    
+//    enum CodingKeys: String, CodingKey {
+//        case quote = "q"
+//        case authorName = "a"
+//        case formattedQuote = "h"
+//    }
+}
